516e7d2fa792e57a8779e2d0d5536494e86bf7fa
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-05-06 09:15:44 +0530

    Fixed linter errors

--- morse_code.rb
+++ morse_code.rb
@@ -34,8 +34,8 @@ end
 def decode_word(word)
   string = ''
   word_array = word.split
-  word_array.each { |el| string+= MORSE_CODE_MAP[el] }
-  return string
+  word_array.each { |el| string += MORSE_CODE_MAP[el] }
+  string
 end
 
 # decode_word('-- -.--')
@@ -43,10 +43,10 @@ end
 def decode(sentence)
   message = ''
   word_array = sentence.split('  ')
-  word_array.each { |word|
-    message += decode_word(word) + ' '
-  }
+  word_array.each do |word|
+    message += "#{decode_word(word)} "
+  end
   message
 end
 
-p decode('.-   -... --- -..-   ..-. ..- .-.. .-..    --- ..-.    .-. ..- -... .. . ...')
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..    --- ..-.    .-. ..- -... .. . ...')
